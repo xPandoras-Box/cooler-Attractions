6c3540fcae1b5cb92eb3fd8afc91301c0fb20d64
Francesca G <75336760+xPandoras-Box@users.noreply.github.com>
2022-03-03 08:55:44 -0700

    Add files via upload

---
+++ coolerAttractions/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coolerAttractions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void attractionsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.attractionsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database7DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database7DataSet1.Attractions' table. You can move, or remove it, as needed.
+            this.attractionsTableAdapter1.Fill(this.database7DataSet1.Attractions);
+            // TODO: This line of code loads data into the 'database7DataSet.Attractions' table. You can move, or remove it, as needed.
+            this.attractionsTableAdapter.Fill(this.database7DataSet.Attractions);
+
+        }
+    }
+}
